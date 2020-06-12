8651aea7ddecb31fc787511f11763a074b1683ed
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-11 22:17:49 -0700

    Enable sessions and secure session password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
 
   set :views, ->{ File.join(root, '../views')}
   enable :sessions
-  set :session_secret, ENV['SECRET_SESSION']
+  set :session_secret, ENV['SECRET_SESSION_PW']
 
   get '/' do
     erb :index
--- config/env.rb
+++ config/env.rb
@@ -3,6 +3,10 @@ ENV['SINATRA_APP'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_APP'])
 
+if ENV['SINATRA_APP'] == 'development'
+  require_relative '../secrets'
+end
+
 configure :development do
   set :database, {adapter: "sqlite3", database: "db/db.sqlite3"}
 end
