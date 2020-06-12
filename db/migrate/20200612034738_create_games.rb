fe6cb79a7be48ba54680212e66508d2f10e69b19
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-11 20:55:15 -0700

    Finish migration for both tables

--- db/migrate/20200612034738_create_games.rb
+++ db/migrate/20200612034738_create_games.rb
@@ -1,4 +1,10 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
+    create_table :games do |t|
+      t.string :name
+      t.string :genre
+      t.string :reason
+      t.integer :user_id
+    end
   end
 end
