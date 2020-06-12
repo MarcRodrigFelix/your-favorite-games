e99cf5a918806f2895b21b6ebad52b20b73c7cae
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-11 22:04:58 -0700

    Add gitignore file with secrets file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < Sinatra::Base
 
   set :views, ->{ File.join(root, '../views')}
+  enable :sessions
+  set :session_secret, ENV['SECRET_SESSION']
 
   get '/' do
     erb :index
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-User.create(username: "Marky", email: "marky@marky.com", password: "marky")
-User.create(username: "Vane", email: "vane@vane.com", password: "vane")
-User.create(username: "Laya", email: "laya@laya.com", password: "laya")
-User.create(username: "Gabe", email: "gabe@gabe.com", password: "gabe")
-User.create(username: "Murphy", email: "murphy@murphy.com", password: "murphy")
-User.create(username: "Casey", email: "casey@casey.com", password: "casey")
-User.create(username: "Sonie", email: "sonie@sonie.com", password: "sonie")
+a = User.create(username: "Marky", email: "marky@marky.com", password: "marky")
+b = User.create(username: "Vane", email: "vane@vane.com", password: "vane")
+c = User.create(username: "Laya", email: "laya@laya.com", password: "laya")
+d = User.create(username: "Gabe", email: "gabe@gabe.com", password: "gabe")
+e = User.create(username: "Murphy", email: "murphy@murphy.com", password: "murphy")
+f = User.create(username: "Casey", email: "casey@casey.com", password: "casey")
+g = User.create(username: "Sonie", email: "sonie@sonie.com", password: "sonie")
 
 
 # Game.create(name: "God of War", genre: "Action", reason: "Father and son experience", user_id)
