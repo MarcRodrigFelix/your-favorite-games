edd8971e0d72aed956dc0b7c805dd09f5835e295
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-12 00:14:38 -0700

    Created controllers for signup page and signup form, can create user with signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
 
+  get '/login' do
+    @users = User.all
+    erb :home
+  end
+
   get '/signup' do
     erb :signup
   end
 
   post '/signup' do
-    binding.pry
-    
+    if !!session[:user_id]
+      erb :home
+    end
+
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+    erb :home
   end
 
 end
