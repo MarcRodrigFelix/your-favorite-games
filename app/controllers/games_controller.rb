070beb510e51a6eb13a97539f5c0cf106ca43351
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-13 00:13:44 -0700

    Add functionality to new erb form to add new game and redirect back home to show updated list

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,17 @@
 class GamesController < ApplicationController
   
-  get '/new' do
-  binding.pry
+  get '/new.erb' do
     erb :'games/new'
   end
 
   post '/new' do
-binding.pry
-    current_user
-
-    erb :'users/home'
+    @game = Game.new(name: params[:name], genre: params[:genre], reason: params[:reason])
+    @game.user = current_user
+    if @game.save
+      redirect '/home'
+    else
+      erb :'games/new'
+    end    
   end
 
 end
