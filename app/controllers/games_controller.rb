36ba7aa55d7b84285129860bbf63a2b260bd0a63
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-13 20:39:34 -0700

    Changed erb name too new_game from new to add new game to list

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,16 @@
 class GamesController < ApplicationController
   
-  get '/new.erb' do
-    erb :'games/new'
+  get '/new_game' do
+    erb :'games/new_game'
   end
 
-  post '/new' do
+  post '/new_game' do
     @game = Game.new(name: params[:name], genre: params[:genre], reason: params[:reason])
     @game.user = current_user
     if @game.save
       redirect '/home'
     else
-      erb :'games/new'
+      erb :'games/new_game'
     end    
   end
 
