06e471d59854bd3be2af7ab6f59caf497e28b676
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-16 00:19:31 -0700

    Added lambda to cheeck if user password is blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, uniqueness: true
-  validates :password, length: { in: 6..20 }
+  validates :password, length: { in: 6..20 }, unless: ->(user){ user.password.blank? }
   has_many :games
 end
