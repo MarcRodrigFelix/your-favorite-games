f55b3360f3318fb8dbac53933255a09a5a847191
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-16 21:18:43 -0700

    Changed edit bbutton to href attribute on game show page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -23,7 +23,7 @@ class GamesController < ApplicationController
   end
 
   get '/games/:id' do #loads show.erb to show game details
-    error if current_game.nil?
+    # error if current_game.nil?
     if valid_user?
       authenticate
       current_game
